820dd50c2469e41c3ab295629e1e0aac35d7adea
Noekz <muhammadnuha181@gmail.com>
2025-04-07 06:31:33 +0700

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import { requestToGroqAI } from "./utils/groq";
+import { Light as SyntaxHighlight } from "react-syntax-highlighter";
+import { darcula } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import './App.css';
+
+
+function App() {
+  const [data, setData] = useState("");
+
+
+  const handleSubmit = async () => {
+    const ai = await requestToGroqAI(content.value);
+    setData(ai);
+  };
+  return (
+    <main className="flex flex-col min-h-[80vh] justify-center items-center max-w-xl w-full mx-auto">
+      <h1 className="text-4xl text-indigo-500 font-bold">MAMAD AI by 7NoeKz</h1>
+      <form className="flex flex-col gap-4 py-4 w-full">
+        <input 
+         placeholder="Ketik permintaan disini..."
+         className="py-2 px-4 text-md rounded-md"
+         id="content"
+         type="text"
+        />
+        <button 
+         onClick={handleSubmit}
+         type="button"
+         className="bg-indigo-500 py-2 px-4 font-bold text-white rounded-md">
+          Kirim
+        </button>
+      </form>
+      <div className="max-w-xl w-full mx-auto">
+        {data ? (
+          <SyntaxHighlight language="swift" style={darcula} wrapLongLines={true}>
+            {data}
+          </SyntaxHighlight>
+        ) : null} 
+      </div>
+    </main>
+  );
+}
+
+export default App
